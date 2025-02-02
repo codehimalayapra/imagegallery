3dd86bf32e3f4e1fcaa8aa515e811fd2d016a01e
prabesh <prabesh.pathak@codehimalaya.net>
2025-02-02 20:31:45 +0545

    fix: added text scaling for smaller screen

--- src/features/ImageGallery/components/ImageCard.tsx
+++ src/features/ImageGallery/components/ImageCard.tsx
@@ -22,7 +22,9 @@ export const ImageCard = ({ image }: { image: ImageListResponse }) => {
       <div className="absolute inset-0 bg-gradient-to-t from-black via-transparent to-transparent opacity-70"></div>
 
       <div className="absolute bottom-0 left-0 right-0 p-4 text-white text-center">
-        <p className="text-lg font-semibold drop-shadow-md">{image.author}</p>
+        <p className="text-xs md:text-lg font-semibold drop-shadow-md">
+          {image.author}
+        </p>
       </div>
     </div>
   );
